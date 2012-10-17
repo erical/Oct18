fcb6ab9a21c95a6d5a452d9b38c2fcb98ab11659
Erica Lederman <erical55@hotmail.com>
2012-10-16 20:33:38 -0400

    Initial Commit

---
+++ Oct18/Oct18AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Oct18AppDelegate.h
+//  Oct18
+//
+//  Created by Erica Lederman on 10/16/12.
+//  Copyright (c) 2012 Erica Lederman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Oct18AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Oct18/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Oct18
+//
+//  Created by Erica Lederman on 10/16/12.
+//  Copyright (c) 2012 Erica Lederman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Oct18AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Oct18AppDelegate class]));
+    }
+}
